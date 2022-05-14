01f2879bc2f0428054f62ae2dd16b9e0322a8121
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-14 10:04:45 +0300

    feature:secure passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
@@ -9,9 +10,29 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),nullable=False,unique=True)
     bio = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255),nullable=False,unique=True)
+    password = db.Column(db.String(255),nullable=False,unique=True)
     profile_pic_path = db.Column(db.String())
     comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
     def __repr__(self):
@@ -24,8 +45,14 @@ class Comment(db.Model):
     comment = db.Column(db.String(255))
     users = db.relationship('User', backref='comments',lazy= "dynamic")
 
+
+    
+    
+    
+    
     def __repr__(self):
         return f'User {self.comment}'
+       
 
 
 
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/quotes'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogs'
 
 class ProdConfig(Config):
     pass
