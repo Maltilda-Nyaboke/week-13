f4f8a3157c01bcc069a0f8cd6dc223d29b6cc6f0
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-14 09:10:35 +0300

    style:add comments model

--- app/models.py
+++ app/models.py
@@ -12,5 +12,11 @@ class User(db.Model):
         return f'User {self.username}'
 
 
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.String(255))
+
+
 
     
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/quotes'
 
 class ProdConfig(Config):
     pass
