9c120c7832cb01d7fae8b682454dd45d9e95c257
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-14 08:04:00 +0300

    feature:initilialize class user

--- app/models.py
+++ app/models.py
@@ -1,4 +1,9 @@
 
-
+class User:
+    def __init__(self,name,username,password,email):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.email = email
 
     
