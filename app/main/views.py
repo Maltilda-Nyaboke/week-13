739dbcf8e3628858cabe60ef81202a50e8b51bac
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-15 20:36:37 +0300

    feature:displaying blogs

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,8 @@ from ..models import Blog, User
 @main.route('/')
 @login_required
 def index():
-    return render_template('index.html',user= current_user)
+    blogs = Blog.query.all()
+    return render_template('index.html',user= current_user,blogs=blogs)
 
 @main.route('/new_blog <id>',methods=['POST', 'GET']) 
 @login_required
@@ -19,6 +20,7 @@ def new_blog(id):
         blog = Blog(blog=blog,writer=current_user.id)
         db.session.add(blog)
         db.session.commit()
+        return redirect(url_for(')main.index'))
     return render_template('blog.html',user= current_user) 
 
 
