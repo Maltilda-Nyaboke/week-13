5d26d238d9e5674077d31a60a50c49f65eb401a7
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-15 06:11:23 +0300

    feature:loader function

--- app/models.py
+++ app/models.py
@@ -1,15 +1,18 @@
+from flask_login import UserMixin
 from . import db
+from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),nullable=False,unique=True)
-    bio = db.Column(db.String(255))
     password = db.Column(db.String(255),nullable=False,unique=True)
     profile_pic_path = db.Column(db.String())
     comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
