8968529aaecc23d195aa58918670ee789d12da0b
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-15 16:31:15 +0300

    style:initialize class blog

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from flask_login import UserMixin
 from . import db
 from . import login_manager
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -26,4 +26,7 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Blog(db.Model):
+
 
