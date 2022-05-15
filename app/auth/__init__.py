843db28d93b79c7f601c6b27a306f163b70f4792
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-15 06:35:32 +0300

    patch:auth error

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,15 +1,5 @@
 from flask import Blueprint
-from flask import render_template
-from . import auth
 from . import views,forms
-
-
 auth = Blueprint('auth',__name__)
-
-
-
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
 
 
