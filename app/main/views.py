ec307e1ea6a2fefa1b4666d9b1abbc9e47208397
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-16 03:36:10 +0300

    feature:finalize on add comment function

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from .. import db
 from flask_login import login_required,current_user
-from ..models import Blog, User
+from ..models import Blog, User,Comment
 
 
 
@@ -37,6 +37,18 @@ def blogs(username):
     blogs=user.blogs
 
     return render_template('blog_display.html',user= current_user,blogs=blogs,username=username)
+
+
+@main.route('/create_comment/<blog_id>',methods=['POST'])  
+@login_required
+def create_comment(blog_id):
+    text = request.form.get('text')
+    blog = Blog.query.filter_by(id = blog_id)
+    if blog:
+        comment = Comment(text=text,writer = current_user.id ,blog_id=blog_id) 
+        db.session.add(comment)
+        db.session.commit() 
+    return redirect(url_for('main.index'))
        
 
   
