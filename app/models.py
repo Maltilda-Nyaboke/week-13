be24a7e76a2d53adae17d1c6f434ff08499cc965
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-16 03:43:46 +0300

    style:add tablenames

--- app/models.py
+++ app/models.py
@@ -32,17 +32,43 @@ class User(UserMixin,db.Model):
         return f'User {self.username}'
 
 class Blog(db.Model):
+    __tablename__ = 'blogs'
     id = db.Column(db.Integer,primary_key = True)
     blog = db.Column(db.String(255))
     writer = db.Column(db.Integer,db.ForeignKey('users.id',ondelete="CASCADE"),nullable=False)
     comments = db.relationship('Comment', backref='blogs',passive_deletes= True,lazy = 'dynamic')
 
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_blog(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return f'User {self.blog}'
+
 
 class Comment(db.Model): 
+    __tablename__ = 'comments'
     id = db.Column(db.Integer,primary_key = True)
     comment = db.Column(db.String(255))
     writer = db.Column(db.Integer,db.ForeignKey('users.id',ondelete="CASCADE"),nullable=False)
     blog_id= db.Column(db.Integer,db.ForeignKey('blogs.id',ondelete="CASCADE"),nullable=False)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return f'User {self.comment}'
 
    
 
